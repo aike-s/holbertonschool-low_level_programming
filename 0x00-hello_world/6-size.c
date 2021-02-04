fe0b4f95021a117660b12d3537a05fc28167dc3c
aike-s <sagrilo804@gmail.com>
2021-02-03 21:37:30 -0500

    Add-files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,10 @@
  */
 int main(void)
 {
-char charType;
-int intType;
-long longint;
-long long longlongint;
-float floatType;
-printf("Size of char: %zu byte (s) \n", sizeof(charType));
-printf("Size of int: %zu byte (s) \n", sizeof(intType));
-printf("Size of a long int: %i byte (s) \n", sizeof(longint));
-printf("Size of a long long int: %i byte (s) \n", sizeof(longlongint));
-printf("Size of float: %zu byte (s) \n", sizeof(floatType));
+printf("Size of char: %zu byte (s) \n", sizeof(char));
+printf("Size of int: %zu byte (s) \n", sizeof(int));
+printf("Size of a long int: %i byte (s) \n", sizeof(long int));
+printf("Size of a long long int: %i byte (s) \n", sizeof(long long int));
+printf("Size of float: %zu byte (s) \n", sizeof(float));
 return (0);
 }
