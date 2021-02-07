607cc6a83d03f6f05c316c3ffe72a51e7d6eba89
aike-s <sagrilo804@gmail.com>
2021-02-06 20:55:32 -0500

    add-file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+	for (n = 'z'; n <= 'a'; n--)
+	{
+		putchar (n);
+	}
+	return (0);	
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a = (48);
+	char b = (97);
+
+	for (a = (48); a <= (57); a++)
+	{
+		putchar (a);
+	}
+	for (b = (97); b <= (102); b++)
+	{
+		putchar (b);
+	}
+	return (0);	
+}
