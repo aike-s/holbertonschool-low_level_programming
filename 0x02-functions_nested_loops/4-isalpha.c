2982e094c58ab9c9458f30ac6a12078f05abb0a1
aike-s <sagrilo804@gmail.com>
2021-02-09 14:35:55 -0500

    fix description

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * _putchar - print a character on the screen
- *
- *@c: char ascii in int
- *
- * Return: on success 1
- *
- */
+*_isalpha - checks for alphabetic character
+*
+*@c: char ascii in int
+*
+* Return: on success 1
+*
+**/
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
