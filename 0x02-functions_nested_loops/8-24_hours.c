f1e0a486cf89d9413aed6685b4e8e6c530bc5c4d
aike-s <sagrilo804@gmail.com>
2021-02-09 12:45:27 -0500

    add-file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _putchar - print a character on the screen
+ *
+ *@c: char ascii in int
+ *
+ * Return: on success 1
+ *
+ */
+void jack_bauer(void)
+{
+	short minutes;
+	short hours;
+
+	for (hours = 0; hours <= 23; hours++)
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar ((minutes / 10) + '0');
+			_putchar ((minutes % 10) + '0');
+			_putchar (':');
+			_putchar ((minutes / 10) + '0');
+			_putchar ((minutes % 10) + '0');
+			_putchar ('\n');
+		}
+}
