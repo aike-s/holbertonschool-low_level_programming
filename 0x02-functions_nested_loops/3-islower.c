94426b8638b96398041857af633984fe829dc3de
aike-s <sagrilo804@gmail.com>
2021-02-09 14:07:51 -0500

    space in description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- * _putchar - print a character on the screen
- *
- *@c: char ascii in int
- * 
- *Return: on success 1
- */
+* _putchar - print a character on the screen
+*
+*@c: char ascii in int
+* 
+*Return: on success 1
+**/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
