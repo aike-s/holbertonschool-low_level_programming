e4871d81639f4e0c71ad6f7b57448b03fc93db93
aike-s <sagrilo804@gmail.com>
2021-02-09 14:48:44 -0500

    description

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* jack_bauer -prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+* jack_bauer -prints every minute of the day from 00:00 to 23:59
 *
 * Return: always void
 *
