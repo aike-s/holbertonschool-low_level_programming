85c476949cbe515eb389e7ba1ee816c5cf413a7b
aike-s <sagrilo804@gmail.com>
2021-02-09 14:44:19 -0500

    fix second for

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 	for (hours = 0; hours <= 23; hours++)
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
-			_putchar ((minutes / 10) + '0');
-			_putchar ((minutes % 10) + '0');
-			_putchar (':');
-			_putchar ((minutes / 10) + '0');
-			_putchar ((minutes % 10) + '0');
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar ('\n');
 		}
 }
