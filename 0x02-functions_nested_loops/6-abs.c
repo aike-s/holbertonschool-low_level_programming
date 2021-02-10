22b454bde308f8c1410e46ed3a55363fb6679475
aike-s <sagrilo804@gmail.com>
2021-02-09 19:48:05 -0500

    multiplicar negativo x negativo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _abs - computes the absolute value of an integer
+*@a:pe 
+*Return: a
+*
+**/
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
