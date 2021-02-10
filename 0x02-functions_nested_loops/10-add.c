3f64851b05783678b1e5adb0cc4cf7a3defe3a40
aike-s <sagrilo804@gmail.com>
2021-02-10 17:08:24 -0500

    put parameters

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*add -adds two integers and returns the result
+*@a: parameter1
+*@b: parameter2
+*Return: 
+*
+**/
+int add(int a, int b)
+{
+	int suma = a + b;
+
+	return (suma);
+}
