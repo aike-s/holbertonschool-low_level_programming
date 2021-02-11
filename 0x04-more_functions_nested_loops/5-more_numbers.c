7a2b7d2940ec9bffcc6ab73e9d05183df26bc9a2
aike-s <sagrilo804@gmail.com>
2021-02-11 15:27:05 -0500

    add code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,12 @@
 #include "holberton.h"
 /**
- *more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line
- *Return: always 0
- *two_digits - prints two digits in one character
+ *more_numbers - prints 10 times the numbers, from 0 to 14
  *Return: always void
  **/
-int two_digits(int)
-{
-	int numero = 0;
-
-	if (numero > 9)
-	{
-		_putchar ('0' + numero / 10);
-		_putchar ('0' + numero % 10);
-	}
-	return (0);
-}
 void more_numbers(void)
 {
 	int numero;
-       	int loop;
+	int loop;
 
 	for (loop = 0; loop <= 9; loop++)
 	{
@@ -28,8 +15,8 @@ void more_numbers(void)
 				_putchar(numero);
 			else
 			{
-				two_digits(numero);
+				_putchar ((numero / 10) + '0');
+				_putchar ((numero % 10) + '0');
 			}
-		_putchar('\n');
 	}
 }
