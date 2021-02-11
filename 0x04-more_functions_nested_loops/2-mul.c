8e6e42dfb6e589285890b75a31348256e93142ba
aike-s <sagrilo804@gmail.com>
2021-02-11 10:55:53 -0500

    add code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *mul - multiplies two integers
+ *@a: integral1
+ *@b: integral2
+ *Return: result of m
+ **/
+int mul(int a, int b)
+{
+	int m = a * b;
+
+	return (m);
+}
