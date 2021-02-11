617ba96203963e01069be3d616aa127de3f966f8
aike-s <sagrilo804@gmail.com>
2021-02-11 10:48:45 -0500

    add code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_isdigit - checks for a digit (0 through 9)
+ *@c: parameter of variable
+ *Return: on success 1, else 0
+ **/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
