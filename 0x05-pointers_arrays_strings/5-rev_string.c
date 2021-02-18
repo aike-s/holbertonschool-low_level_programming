99894534106b5613172d5c05823f5b461eef3d99
aike-s <sagrilo804@gmail.com>
2021-02-17 22:12:05 -0500

    fix first for, betty style

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@ void rev_string(char *s)
 	int length, lengthHalf,  mirror;
 	char temp;
 
-	for (length = 0; s[length]; length++);
+	for (length = 0; s[length]; length++)
+	{}
 	length--;
-	lengthHalf = length / 2; /*se divide la longtud*/
+	lengthHalf = length / 2;
 	for (mirror = 0; mirror < lengthHalf; mirror++, length--)
 	{
 		temp = s[mirror];
 		s[mirror] = s[length];
-		s[length] = temp; /*se cambian los valores de las variables*/
+		s[length] = temp;
 	}
 }
