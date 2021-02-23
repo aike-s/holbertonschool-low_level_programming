24239195fd5fde3609a6b4f472cbbc8c00cb577f
aike-s <sagrilo804@gmail.com>
2021-02-22 23:57:22 -0500

    add code

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *print_diagsums - sets the value of a pointer to a char
+ *@a: pointer to pointer
+ *@size: tamaño
+ *Return: always void
+ **/
+void print_diagsums(int *a, int size)
+{
+	int down, right;
+	int first = 0, last = 0, limit = 1;
+
+	for (down = 0; down < size; down++)
+	{
+		for (right = 0; right < size; right++)
+		{
+			if (down == right)
+				first = (*(a + ((s * size) + j)));
+			if ((size - limit) == j)
+			{
+				last = (*(a + ((s * size) + j)));
+				limit++;
+			}
+		}
+	}
+}
