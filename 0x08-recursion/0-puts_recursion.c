b2549e0ceccf8c6e3551cf7bb43310166130464a
aike-s <sagrilo804@gmail.com>
2021-02-24 15:21:09 -0500

    add code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - prints a string, followed by a new line
+ *@s: pointer to the string
+ *Return: pointer s
+ **/
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s != '\0')
+		_puts_recursion(s + 1);
+	else
+		_putchar('\n');
+}
