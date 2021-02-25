8499a4019adf5767903861454b2ff1a68c4c1ce7
aike-s <sagrilo804@gmail.com>
2021-02-24 23:39:13 -0500

    add code

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *wildcmp - compares two strings
+ *@s2: pointer2
+ *@s1: pointer1
+ *Return: always void
+ **/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 != '*')
+		return (0);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
