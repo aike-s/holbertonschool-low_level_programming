14ec6e1783ef8e6f9623bbd8813ba6ea13a297a5
aike-s <sagrilo804@gmail.com>
2021-02-27 15:09:26 -0500

    add code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - program that prints its name
+ *@argv: pointer to the string
+ *@argc: number of the word in the string
+ *Return: the name of the string
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[i]; i++)
+		printf("%s\n", argv[i]);
+	(void)argc;
+	return (0);
+}
