6a5982fcff2cde1ae177186f5fb27c6e39231d41
aike-s <sagrilo804@gmail.com>
2021-02-27 16:31:23 -0500

    add code

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *main - program that prints its name
+ *@argv: pointer to the string
+ *@argc: number of the word in the string
+ *Return: the name of the string
+ **/
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
