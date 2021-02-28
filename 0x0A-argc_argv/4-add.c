5ea1f557f3205afd5d454a3aa8911cc5456ab807
aike-s <sagrilo804@gmail.com>
2021-02-27 21:00:04 -0500

    add code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,25 +9,22 @@
  **/
 int main(int argc, char *argv[])
 {
-	int i = 1;
+	int i;
 	int sum = 0;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if(*argv[i] >= 'a' && *argv[i] <= 'z')
+			if (!(*argv[i] >= '0' && *argv[i] <= '9'))
 			{
 				printf("Error\n");
 				return (1);
 			}
 			else
-			{
 				sum += atoi(argv[i]);
-				printf("%d\n", sum);
-				return (0);
-			}
 		}
+		printf("%d\n", sum);
 	}
 		else
 			printf("0\n");
