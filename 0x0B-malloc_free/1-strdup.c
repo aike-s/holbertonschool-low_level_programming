49259c82cef7886923a7486743f74bcaf76e5f07
aike-s <sagrilo804@gmail.com>
2021-03-02 16:35:17 -0500

    fix first if with another condition

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,11 @@ char *_strdup(char *str)
 	char *ptr;
 	int i, j;
 
-	if (str == NULL)
-		return (NULL);
 	for (i = 0; *(str + i); i++)
 	{}
 	ptr = malloc(sizeof(*ptr) * i); /*here the space for the string is separated*/
+	if (str == NULL || ptr == NULL)
+		return (NULL);
 	for (j = 0; *(str + j); j++)
 		*(ptr + j) = *(str + j);
 	return (ptr);
