c6ebbf9d3e3bb3ce0218ed7afc001d1f835710d3
aike-s <sagrilo804@gmail.com>
2021-03-03 10:54:42 -0500

    fix length of s1 and s2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,17 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int j, i;
 	char space = 0;
-	char len1 = sizeof(*s1);
-	char len2 = sizeof(*s2);
+	char len1 = 0;
+	char len2 = 0;
 
 	if (s1 == NULL)
 		s1 = ("");
 	if (s2 == NULL)
 		s2 = ("");
+	for (i = 0; s1[i]; i++)
+		len1++;
+	for (j = 0; s2[j]; j++)
+		len2++;
 	space = len1 + len2;
 	ptr = malloc(sizeof(*ptr) * space + 1);
 	if (ptr == NULL)
