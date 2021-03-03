c0a81b031406b65a912a7ec59c8e88b3b3a1f39b
aike-s <sagrilo804@gmail.com>
2021-03-02 19:32:24 -0500

    add ascii in first and second if

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int j, i;
 	char space = 0;
-	char len1 = sizeof(*s1) - 1;
-	char len2 = sizeof(*s2) - 1;
+	char len1 = sizeof(*s1);
+	char len2 = sizeof(*s2);
 
 	if (s1 == NULL)
-		s1 = 0;
+		s1 = 00;
 	if (s2 == NULL)
-		s2 = 0;
+		s2 = 00;
 	space = len1 + len2;
 	ptr = malloc(sizeof(*ptr) * space + 1);
 	if (ptr == NULL)
