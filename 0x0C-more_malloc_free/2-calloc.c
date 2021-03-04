2ac40e636185a3401ab347227197491b777feaa3
aike-s <sagrilo804@gmail.com>
2021-03-04 15:53:36 -0500

    fix the initialization in 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,18 +3,24 @@
 #include <stdlib.h>
 /**
  *_calloc - allocates memory for an array
- *@nmemb: number of memory
+ *@nmemb: number of elements
  *@size: size of the spaces in the memory
  *Return: always void
  **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL) /*when malloc fails then malloc is equal to NULL*/
 		return (NULL);
-	return (ptr);
+	while (i <= nmemb)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return ((void *)ptr);
 }
