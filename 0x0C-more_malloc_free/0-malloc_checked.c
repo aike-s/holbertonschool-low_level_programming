ff21ae227795d0899856d10a45250fc77a74550b
aike-s <sagrilo804@gmail.com>
2021-03-04 11:19:53 -0500

    add code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *malloc_checked - allocates memory
+ *@b:
+ *Return: a pointer to the allocated memory
+ **/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
