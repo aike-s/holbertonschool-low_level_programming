2ce49a7ea1baa7b3e2f446eef7934728c1786a6a
aike-s <sagrilo804@gmail.com>
2021-03-05 11:16:48 -0500

    fix again parameters in for

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	ptr = malloc(sizeof(int) * space);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i <= space; i++, min++)
+	for (i = 0; i < space; i++, min++)
 		*(ptr + i) = min;
 	return (ptr);
 }
