b1d6eec7d38ba190b0becd08995923376ff9cbf8
aike-s <sagrilo804@gmail.com>
2021-03-08 18:36:52 -0500

    fix the coditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		d->name = "nil";
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		d->owner = "nil";
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
