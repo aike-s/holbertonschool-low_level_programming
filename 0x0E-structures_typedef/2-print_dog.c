5d0769412610d04630aab178ff06fd39bee5a3f7
aike-s <sagrilo804@gmail.com>
2021-03-08 18:30:44 -0500

    fix second printf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)");
 	if (d->owner == NULL)
-		printf("owner: (nil)");
+		printf("Owner: (nil)");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
