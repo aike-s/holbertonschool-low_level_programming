19494a96e7169d9c58be99b5f4428ea73b661d38
aike-s <sagrilo804@gmail.com>
2021-03-08 20:54:31 -0500

    add code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *free_dog - frees dogs
+ *@d: doggy
+ **/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
