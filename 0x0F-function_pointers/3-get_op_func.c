847c48360f2a9bf6dba10be232365c75410d9534
aike-s <sagrilo804@gmail.com>
2021-03-11 00:01:24 -0500

    add code

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i] .op)
 	{
 		if (ops[i].op[0] == s[0] && s[1] == '\0')
-			return (ops[i].f)
+			return (ops[i].f);
 		i++;
 	}
 	return (NULL);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,16 +15,16 @@ int main(int ac, char *av[])
 		exit(98);
 	}
 	if ((av[2][0] == '/' || av[2][0] == '%')
-	    && arv[2][1] == '\0' && av[3][0] == '0')
+	    && av[2][1] == '\0' && av[3][0] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(atoi(av[1]), atoi(av[3])) == NULL)
+	if (get_op_func(av[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", get_op_func(atoi(av[1]), atoi(av[3])));
+	printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
 	return (0);
 }
