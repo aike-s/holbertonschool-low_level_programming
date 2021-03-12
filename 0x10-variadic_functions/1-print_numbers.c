11b09ab81bde223d65f1234af0c5b778e268d175
aike-s <sagrilo804@gmail.com>
2021-03-11 20:10:18 -0500

    put new condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,19 +11,18 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list(arg);
-	unsigned int i, j;
+	unsigned int i;
 
 	if (n == 0)
 		return;
+	if (separator == NULL)
+		separator = "";
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i < (n - 1))
 		{
-			printf("%d", va_arg(arg, int));
-			if (*separator != '\0')
-				for (j = 0; separator[j]; j++)
-					printf("%c", separator[j]);
+			printf("%d%s", va_arg(arg, int), separator);
 		}
 		else
 			printf("%d", va_arg(arg, int));
