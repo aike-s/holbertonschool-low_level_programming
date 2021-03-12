219aa7dcbb8f7873ce7beeaa819a5aa3c8d4d0b6
aike-s <sagrilo804@gmail.com>
2021-03-11 20:17:01 -0500

    remove condition to n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list(arg);
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	if (separator == NULL)
 		separator = "";
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i < (n - 1))
-		{
 			printf("%d%s", va_arg(arg, int), separator);
-		}
 		else
 			printf("%d", va_arg(arg, int));
 	}
