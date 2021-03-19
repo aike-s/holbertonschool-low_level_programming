7e558f69af7e3d1d17da56c370a0400d62c9dcd9
aike-s <sagrilo804@gmail.com>
2021-03-18 21:19:08 -0500

    [Add] code and explanatory comments

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elementos of a list
+ * @h: pointer to the list to print
+ * Return: the number of nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	const list_t *box;
+	size_t num;
+
+	box = h; /*give the values of h to box*/
+	while (box != NULL) /*while the linked list ends*/
+	{
+		if (box->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		/*print what is in len and str*/
+		printf("[%d] %s\n", box->len, box->str);
+		/*pass to the next node*/
+		box = box->next;
+		/*and add one to num each time it advances a node*/
+		num++;
+	}
+	return (num);
+}
