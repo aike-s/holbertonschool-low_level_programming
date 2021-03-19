555af9f6f84267e74d5052dd1540a4c0e90ef97b
aike-s <sagrilo804@gmail.com>
2021-03-19 11:32:49 -0500

    [fix] betty style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -17,7 +16,6 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-	
 } list_t;
 /*prints all the elements of a list_t list*/
 size_t print_list(const list_t *h);
