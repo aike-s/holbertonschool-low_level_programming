7eaa26b89d9fef6fcb9ce5608b6aade1cd07e6dd
aike-s <sagrilo804@gmail.com>
2021-03-19 22:48:23 -0500

    [add] code

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list to free
+ * Return: always void
+ **/
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free(head->next);
+		head = head->next;
+	}
+	free(head);
+}
