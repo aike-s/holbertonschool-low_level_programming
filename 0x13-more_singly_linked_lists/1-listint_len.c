be893a0d279dec0d40461768171d84d8b14c957b
aike-s <sagrilo804@gmail.com>
2021-03-22 17:03:49 -0500

    [Add] code

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to head
+ * Return: the number of nodes
+ **/
+size_t listint_len(const listint_t *h)
+{
+		const listint_t *box;
+		size_t num = 0;
+
+		box = h; /*give the values of h to box*/
+		if (box == NULL)
+			return (0);
+		while (box != NULL) /*while the linked list ends*/
+		{
+			/*pass to the next node*/
+			box = box->next;
+			/*and add one to num each time it advances a node*/
+			num++;
+		}
+		return (num);
+}
