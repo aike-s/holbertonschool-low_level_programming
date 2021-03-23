52604044a41ee13d74069e9cbe981f27266da5e2
aike-s <sagrilo804@gmail.com>
2021-03-23 14:30:14 -0500

    [add] code

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the list
+ * Return: always void
+ **/
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	free_listint((*head)->next);
+	free(*head);
+	*head = NULL;
+}
