fcad88bbf9223730edd7476e18128f1c150895de
aike-s <sagrilo804@gmail.com>
2021-03-29 12:37:19 -0500

    [Add] templante for header file

--- holberton.h
+++ holberton.h
@@ -1,32 +1,18 @@
-/*writes the character c to stdout*/
-int _putchar(char c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#inlude <stdio.h>
+/**/
 
-/*takes a pointer to an int as parameter and change the value*/
-void reset_to_98(int *n);
+/**/
 
-/*swaps the values of two integers*/
-void swap_int(int *a, int *b);
+/**/
 
-/*returns the length of a string*/
-int _strlen(char *s);
+/**/
 
-/*prints a string, followed by a new line, to stdout*/
-void _puts(char *str);
+/**/
 
-/*prints a string, in reverse*/
-void print_rev(char *s);
+/**/
 
-/*reverses a string*/
-void rev_string(char *s);
+/**/
 
-/*prints every other character of a string*/
-void puts2(char *str);
-
-/*prints half of a string*/
-void puts_half(char *str);
-
-/*prints n elements of an array of integers*/
-void print_array(int *a, int n);
-
-/*copies the string pointed to by src*/
-char *_strcpy(char *dest, char *src);
+#endif
