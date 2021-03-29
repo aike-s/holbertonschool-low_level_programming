90aa9dbc802e1e7e26b45e01830d4622afea9b44
aike-s <sagrilo804@gmail.com>
2021-03-29 12:45:01 -0500

    [add] dir and files

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+/*reads a text file and prints it to the POSIX standard output*/
+size_t read_textfile(const char *filename, size_t letters);
+/*creates a file*/
+int create_file(const char *filename, char *text_content);
+/*appends text at the end of a file*/
+int append_text_to_file(const char *filename, char *text_content);
+/*writes the character c to stdout*/
+int _putchar(char c);
+#endif
