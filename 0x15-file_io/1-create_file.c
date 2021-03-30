a7a16576d2b5ed588e9135bb749b96b068acc792
aike-s <sagrilo804@gmail.com>
2021-03-29 20:00:35 -0500

    [fix] if conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	if (length != 0)
+	if (text_content)
 	{
 		size = write(fd, text_content, length);
 		if (size < 0)
