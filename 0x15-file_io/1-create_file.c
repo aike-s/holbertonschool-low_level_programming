510d987293681fc1acb8d184935342104cfebf50
aike-s <sagrilo804@gmail.com>
2021-03-29 19:46:19 -0500

    [add] oflag to open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	length = _strlen(text_content);
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	if (length != 0)
