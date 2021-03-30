1d87b11bab740af356b40581d49508174005a0a9
aike-s <sagrilo804@gmail.com>
2021-03-29 21:20:15 -0500

    [add] write function and correct the return type

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /*reads a text file and prints it to the POSIX standard output*/
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 /*creates a file*/
 int create_file(const char *filename, char *text_content);
 /*appends text at the end of a file*/
