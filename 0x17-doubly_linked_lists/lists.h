b309f3b1b76b6b32f9ddef47e5c946ce09e381c7
aike-s <sagrilo804@gmail.com>
2021-04-30 11:24:40 -0500

    [add] struct for double linked lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/*FUNCTIONS*/
 /*deletes the node at index index of a dlistint_t linked list*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 /*inserts a new node at a given position*/
