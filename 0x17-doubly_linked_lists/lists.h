a2e5c0c5c92eb4c93834d866de110ae48198f09c
aike-s <sagrilo804@gmail.com>
2021-04-30 11:22:19 -0500

    [add] directory, empty files, header and README file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+/*deletes the node at index index of a dlistint_t linked list*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+/*inserts a new node at a given position*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+/*returns the sum of all the data (n) of a dlistint_t linked list*/
+int sum_dlistint(dlistint_t *head);
+/*returns the nth node of a dlistint_t linked list*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+/*frees a dlistint_t list*/
+void free_dlistint(dlistint_t *head);
+/*adds a new node at the end of a dlistint_t list*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/*adds a new node at the beginning of a dlistint_t list*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/*returns the number of elements in a linked dlistint_t list*/
+size_t dlistint_len(const dlistint_t *h);
+/*prints all the elements of a dlistint_t list*/
+size_t print_dlistint(const dlistint_t *h);
+#endif
