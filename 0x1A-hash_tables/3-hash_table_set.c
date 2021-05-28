3353c1ada5626e80db6c734e392ab75181cc2794
aike-s <sagrilo804@gmail.com>
2021-05-28 15:37:01 -0500

    [add] add_node function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,28 @@
 #include "hash_tables.h"
 /**
+ * add_node - adds a new node at the beginning of a list
+ * @head: the link for de the first
+ * @str: the string to copy
+ * Return: the address of the new element or NULL if it failed
+ **/
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *hash_table_node;
+
+	if (!head)
+		return (NULL);
+
+	hash_table_node = malloc(sizeof(hash_node_t));
+	if (hash_table_node == NULL)
+		return (NULL);
+
+	hash_table_node->key = (char *)key;
+	hash_table_node->value = strdup(value);
+	hash_table_node->next = *head;
+	*head = hash_table_node;
+	return (*head);
+}
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table you want to add or update the key/value to
  * @key: the key
@@ -24,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* no, it does not exist yet, so we pass the empty head as argument */
 	if (ht->array[index] == NULL)
 	{
-		ht->array[index] = add_nodeint(&head, key, value);
+		ht->array[index] = add_node(&head, key, value);
 			if (ht->array[index] == NULL)
 			/* would mean that there was a failure to create the new node */
 				return (0);
@@ -33,7 +56,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* yes, it already exists, so we pass the existing node as current head */
 	else
 	{
-		ht->array[index] = add_nodeint(&ht->array[index], key, value);
+		ht->array[index] = add_node(&ht->array[index], key, value);
 			if (ht->array[index] == NULL)
 			/* would mean that there was a failure to create the new node */
 				return (0);
