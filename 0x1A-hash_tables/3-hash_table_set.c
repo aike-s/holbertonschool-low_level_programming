79a80f3ee9c25c78e1a131ed0e7baf1d35ffabb5
aike-s <sagrilo804@gmail.com>
2021-05-28 19:21:06 -0500

    [add] conditional to update existing key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,24 +2,27 @@
 /**
  * add_node - adds a new node at the beginning of a list
  * @head: the link for de the first
- * @str: the string to copy
+ * @key: the key
+ * @value: the value associated with the key
  * Return: the address of the new element or NULL if it failed
  **/
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 {
-	hash_node_t *hash_table_node;
+	hash_node_t *tmp_node;
 
 	if (!head)
 		return (NULL);
 
-	hash_table_node = malloc(sizeof(hash_node_t));
-	if (hash_table_node == NULL)
+	tmp_node = malloc(sizeof(hash_node_t));
+	if (tmp_node == NULL)
 		return (NULL);
 
-	hash_table_node->key = (char *)key;
-	hash_table_node->value = strdup(value);
-	hash_table_node->next = *head;
-	*head = hash_table_node;
+	tmp_node->key = strdup((char *)key);
+	/* quizas no estoy tomando bien el caso en el que value llegue vacio */
+	/* IMPORTANTE: aqui se hace un malloc */
+	tmp_node->value = strdup(value);
+	tmp_node->next = *head;
+	*head = tmp_node;
 	return (*head);
 }
 /**
@@ -32,9 +35,9 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *head = NULL;
+	hash_node_t *head = NULL, *tmp_node;
 
-	if (key == NULL)
+	if (key == NULL || (!ht))
 		return (0);
 
 	/* here we assign to index the index in which the element */
@@ -49,16 +52,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		ht->array[index] = add_node(&head, key, value);
 			if (ht->array[index] == NULL)
-			/* would mean that there was a failure to create the new node */
 				return (0);
 	}
 
 	/* yes, it already exists, so we pass the existing node as current head */
 	else
 	{
+		/* here it is checked if the key already existed before to be updated*/
+		tmp_node = ht->array[index];
+		while (tmp_node != NULL)
+		{
+			if (strcmp(tmp_node->key, key) == 0)
+			{	tmp_node->value = strdup((char *)value);
+				return (1);
+			}
+			tmp_node = tmp_node->next;
+		}
+		/* if it did not exist, it will now be added to the list */
 		ht->array[index] = add_node(&ht->array[index], key, value);
 			if (ht->array[index] == NULL)
-			/* would mean that there was a failure to create the new node */
 				return (0);
 	}
 	return (1);
