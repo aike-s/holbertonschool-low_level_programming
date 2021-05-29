50489ce6cc5fa754435dfaba98aaa37077d0b60e
aike-s <sagrilo804@gmail.com>
2021-05-28 19:26:56 -0500

    [add] coonditional

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,27 +1,4 @@
 #include "hash_tables.h"
-
-/**
- * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: dest
- **/
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		if (s2[i] != s1[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
-}
 /**
  * hash_table_get - retrieves a value associated with a key
  * @ht: is the hash table you want to look into
