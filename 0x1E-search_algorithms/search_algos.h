039da3067fb7e11eca1c67d15df975d0b77e5b08
aike-s <sagrilo804@gmail.com>
2021-09-01 11:38:16 -0500

    [add] directory, header, empty files and readme

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+
+/*searches for a value in an array of integers*/
+/*using the Linear search algorithm*/
+int linear_search(int *array, size_t size, int value);
+/*searches for a value in a sorted array of integers*/
+/*using the Binary search algorithm*/
+int binary_search(int *array, size_t size, int value);
+
+#endif
